2a4e734b384fe393fde1a2d0ebf52c2441f159cc
githubDlyaHuesosovI314Idarasov <thepokemontrainergold@gmail.com>
2026-02-16 23:14:39 +0200

    Adding few frameworks

--- WebStoreApp.Server/Program.cs
+++ WebStoreApp.Server/Program.cs
@@ -1,38 +1,59 @@
 using DAL.EF;
+using Serilog;
 using Microsoft.EntityFrameworkCore;
 using Npgsql.EntityFrameworkCore.PostgreSQL;
 using StackExchange.Redis;
 using WebStorePrototype.Server.Extensions;
 using Microsoft.Extensions.Caching.StackExchangeRedis;
 
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Verbose()
+    .WriteTo.Console()
+    .CreateLogger();
+
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-builder.Services.AddOpenApi();
-builder.Services.AddWebStoreDBContext(builder.Configuration);
-builder.Services.AddExternalWebStoreDBContext(builder.Configuration);
-builder.Services.AddStackExchangeRedisCache(x => x.ConfigurationOptions = new ConfigurationOptions()
+try
 {
-    EndPoints = { builder.Configuration.GetConnectionString("Redis")! },
-    Password = ""
-});
-var app = builder.Build();
+    Log.Information("Starting web server");
 
-app.UseDefaultFiles();
-app.MapStaticAssets();
+    builder.Services.AddLogging();
+    builder.Logging.AddSeq();
+    builder.Logging.AddSerilog();
+    builder.Services.AddControllers();
+    builder.Services.AddOpenApi();
+    builder.Services.AddWebStoreDBContext(builder.Configuration);
+    builder.Services.AddExternalWebStoreDBContext(builder.Configuration);
+    builder.Services.AddStackExchangeRedisCache(x => x.ConfigurationOptions = new ConfigurationOptions()
+    {
+        EndPoints = { builder.Configuration.GetConnectionString("Redis")! },
+        Password = ""
+    });
+    var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.UseExceptionHandler("/error");
+    app.UseDefaultFiles();
+    app.MapStaticAssets();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+        app.UseExceptionHandler("/error");
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.MapFallbackToFile("/index.html");
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.MapFallbackToFile("/index.html");
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}

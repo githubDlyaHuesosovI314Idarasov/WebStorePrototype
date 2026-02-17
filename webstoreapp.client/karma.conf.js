809a120166457e937cd558b19371126735fbee85
githubDlyaHuesosovI314Idarasov <thepokemontrainergold@gmail.com>
2026-02-17 13:36:54 +0200

    karma config updated

--- webstoreapp.client/karma.conf.js
+++ webstoreapp.client/karma.conf.js
@@ -1,44 +1,31 @@
-﻿module.exports = function (config) {
+const { join } = require('path');
+
+/** @type { import("karma").Config } */
+module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['jasmine', '@angular/build/karma'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular/build/plugins/karma'),
     ],
     client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      jasmine: {},
+      clearContext: false,
     },
     jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+      suppressAll: true,
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/'),
+      dir: require('path').join(__dirname, './coverage/webstoreapp.client'),
       subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
+      reporters: [{ type: 'html' }, { type: 'text-summary' }],
     },
     reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false,
     restartOnFileChange: true,
-    listenAddress: 'localhost',
-    hostname: 'localhost'
   });
 };
-
